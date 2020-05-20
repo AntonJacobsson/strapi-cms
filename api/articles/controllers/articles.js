c53a28fac1b2c09d2861e7aef393815d925fbf7d
Anton Jacobsson <ajacobsson98@gmail.com>
2020-05-20 20:04:37 +0200

    feat(api): add comments and change article controller

--- api/articles/controllers/articles.js
+++ api/articles/controllers/articles.js
@@ -1,8 +1,25 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
-
-module.exports = {};
+module.exports = {
+    async find(ctx) {
+        let entities;
+        if (ctx.query._q) {
+          entities = await strapi.services.articles.search(ctx.query);
+        } else {
+          entities = await strapi.services.articles.find(ctx.query);
+        }
+        var result = entities.map(entity => sanitizeEntity(entity, { model: strapi.models.articles }));
+        result.forEach(element => {
+            element.comments = []
+        });
+        return result;
+    },
+    async findOne(ctx) {
+        const { id } = ctx.params;
+    
+        const entity = await strapi.services.articles.findOne({ id });
+        var result = sanitizeEntity(entity, { model: strapi.models.articles });
+        result.comments = result.comments.filter(x => x.isPublished === true);
+        return result;
+      }
+}
